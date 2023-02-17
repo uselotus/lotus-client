349f7c7db9217d56c7ae534ebbeaa1ae71816442
Timothy Ogbemudia <ogbemudiatimothy@gmail.com>
2023-02-17 16:02:09 +0100

    chore: add globals output to UMD

--- rollup.config.js
+++ rollup.config.js
@@ -1,6 +1,8 @@
 import typescript from '@rollup/plugin-typescript'
+
 export default {
   input: 'src/index.ts',
+  external: ['uuid', 'axios', 'axios-retry', 'ms'],
   output: [
     { file: 'dist/index.js', format: 'cjs' },
     { file: 'dist/index.mjs', format: 'es' },
@@ -8,6 +10,12 @@ export default {
       name: 'Lotus-Client-SDK',
       file: 'dist/index.umd.js',
       format: 'umd',
+      globals: {
+        uuid: 'uuid',
+        'axios-retry': 'axiosRetry',
+        ms: 'ms',
+        axios: 'axios',
+      },
     },
   ],
   plugins: [typescript()],
